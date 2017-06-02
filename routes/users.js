83d0faf501e4405d105958db644e5d9670566844
Zachary Levy <zacharyalevy@gmail.com>
2017-06-02 15:25:25 -0400

    sequalize basic for users, links

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models").User;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.findAll().then(users => {
+    console.log(users)
+    res.send(JSON.stringify(users));
+  })
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
