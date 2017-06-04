1b24adf963ed1fa0977bd2756314d88227aaae59
Zachary Levy <zacharyalevy@gmail.com>
2017-06-04 10:36:35 -0400

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -11,39 +11,38 @@ router.get('/', function(req, res, next) {
 
 router.get('/:slug', function(req, res) {
   console.log(req.body)
+  console.log(req.params.slug)
   Link.findOne({where: {slug: req.params.slug}}).then(function (link) {
     console.log(link);
     res.send(
-      `
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>${link.get('title')}</title>
-            <meta name="description" content="${link.get('description')}">
-            <style>
-              html {
-                height: 100%;
-              }
-              body {
-                margin: 0;
-                height: 100%;
-              }
-              iframe {
-                width: 100%;
-                height: 100%;
-                border: 0;
-                margin: 0;
-              }
-            </style>
-          </head>
-          <body>
-            <iframe src="${link.get('url')}"></iframe>
-          </body>
-        </html>
-      `
+      `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>${link.get('title')}</title>
+          <meta name="description" content="${link.get('description')}">
+          <style>
+            html {
+              height: 100%;
+            }
+            body {
+              margin: 0;
+              height: 100%;
+            }
+            iframe {
+              width: 100%;
+              height: 100%;
+              border: 0;
+              margin: 0;
+            }
+          </style>
+        </head>
+        <body>
+          <iframe src="${link.get('url')}"></iframe>
+        </body>
+      </html>`
     )
   });
 });
