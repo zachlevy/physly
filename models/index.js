2842ba02f98c7a6ef3e648ec2404f0f830c2c721
Zachary Levy <zacharyalevy@gmail.com>
2017-06-05 15:19:02 -0400

    debug production db

--- models/index.js
+++ models/index.js
@@ -8,9 +8,12 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+console.log("Connect to database")
 if (config.use_env_variable) {
+  console.log(process.env[config.use_env_variable])
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
+  console.log(config)
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
